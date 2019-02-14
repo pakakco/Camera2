7ff7a787e1a50d626325f26a21d2bd8a66235304
wangzeng <wangzeng@wingtech.com>
2019-02-14 17:39:41 +0800

    camera2 app

---
+++ app/src/main/java/com/android/camera/MainActivity.java
@@ -0,0 +1,25 @@
+package com.android.camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
